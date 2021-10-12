55ff5ca33790991ca1d602a8defd4254486946ef
MuhammadAnggaSaputra <manggas703@gmail.com>
2021-10-12 21:20:22 +0700

    Menambahkan Struktur folder , Routes dan view vidio 3 pertemuan5

---
+++ applications/coba-laravel/resources/views/about.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>WPU Blog | About</title>
+</head>
+<body>
+    <h1>Halaman About</h1>
+    <h3>Muhammad Angga Saputra</h3>
+    <p>manggas703@gmail.com</p>
+    <img src="img/angga.jpg" alt="Muhammad Angga Saputra" width="50%">
+
+</body>
+</html>  
